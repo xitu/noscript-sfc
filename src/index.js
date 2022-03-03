9bfd74453930959135ab39df882d31f2aa65be6e
akira-cn <19511344@qq.com>
2022-03-03 19:41:38 +0800

    remove sfc-loader

--- src/index.js
+++ src/index.js
@@ -61,7 +61,6 @@ function getBlobURL(jsCode) {
 const map = {
   imports: {
     vue: 'https://unpkg.com/vue@3/dist/vue.esm-browser.js',
-    'sfc-loader': 'https://cdn.jsdelivr.net/npm/vue3-sfc-loader/dist/vue3-sfc-loader.esm.js',
   },
   scopes: { },
 };
